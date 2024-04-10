2903c1fdcf8c705ab8b581adc4f6abf2239c42af
Stephen Smith <stephensmith_aus@yahoo.com.au>
2024-04-11 06:45:20 +1000

    24 Update Ticket Form

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,6 +1,22 @@
-const Tickets = () => {
+import prisma from "@/prisma/db";
+import DataTable from "./DataTable";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
+const Tickets = async () => {
+  const tickets = await prisma.ticket.findMany();
+  console.log(tickets);
+
   return (
-    <div>Tickets</div>
-  )
-}
-export default Tickets
+    <div>
+      <Link
+        href="/tickets/new"
+        className={buttonVariants({ variant: "default" })}
+      >
+        New Ticket
+      </Link>
+      <DataTable tickets={tickets} />
+    </div>
+  );
+};
+export default Tickets;
