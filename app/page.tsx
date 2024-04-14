d75a90fa0599479e89ae2f0e1602096b76e24a1a
Stephen Smith <stephensmith_aus@yahoo.com.au>
2024-04-14 10:26:04 +1000

    End of Sn 8 Dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,44 @@
-const Dashboard = () => {
+import DashChart from "@/components/DashChart";
+import DashRecentTickets from "@/components/DashRecentTickets";
+import prisma from "@/prisma/db";
+import { group } from "console";
+
+const Dashboard = async () => {
+  const tickets = await prisma.ticket.findMany({
+    where: {
+      NOT: [{ status: "CLOSED" }],
+    },
+    orderBy: {
+      updatedAt: "desc",
+    },
+    skip: 0,
+    take: 5,
+    include: {
+      assignedToUser: true,
+    },
+  });
+
+  const groupTicket = await prisma.ticket.groupBy({
+    by: ["status"],
+    _count: {
+      id: true,
+    },
+  });
+
+  const data = groupTicket.map(item => {
+    return {
+      name: item.status,
+      total: item._count.id,
+    };
+  });
+
   return (
-    <div>Dashboard</div>
-  )
-}
-export default Dashboard
+    <div>
+      <div className="grid gap-4 md:grid-cols-2 px-2">
+        <div><DashRecentTickets tickets={tickets} /></div>
+        <div><DashChart data={data} /></div>
+      </div>
+    </div>
+  );
+};
+export default Dashboard;
